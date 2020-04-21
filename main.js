78d5549827c0ca2f1b8ca268e65672128866e7bb
evan.m.reynolds95@gmail.com <evanr95@MSI.localdomain>
2020-04-21 16:27:35 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+/* Practice: Fast Food Ordering
+
+You need to write code to represent the process of ordering food at a fast food joint. Your restaurant is Bob's Burgers, and you have some starter code below. The object will have two behaviors that you must define.
+
+The object should have a property named orders that is an array. You will need to define this.
+
+Define a function to represent the behavior of placing an order. It should accept one argument named meal. This argument will be an object. There is starter code provided.
+
+Define a function to represent the behavior of getting all orders. It should return the order property (hint: this). You will need to define this.
+
+Each meal object should have three properties: sandwichType, fries (true or false), and drinkSize. */
+
+const restaurant = {
+  name: "Bob's Burgers",
+  orders: [],
+  placeOrder: function (meal) {
+    this.orders.push(meal)
+  },
+  allOrders: function () {
+    console.table(this.orders)
+  }
+}
+
+const chickenComboMeal = {
+  sandwichType: "Fried Chicken",
+  fries: true,
+  drinkSize: "Large"
+}
+
+const burgerComboMeal = {
+  sandwichType: "Cheese Burger",
+  fries: true,
+  drinkSize: "Large"
+}
+
+const veganComboMeal = {
+  sandwichType: "Black Bean Burger",
+  fries: false,
+  drinkSize: "Large"
+}
+
+// Place an order
+restaurant.placeOrder(chickenComboMeal)
+restaurant.placeOrder(burgerComboMeal)
+restaurant.placeOrder(veganComboMeal)
+
+
+
+// Invoke the function to return the list of all orders
+
+// Output all orders to the console using console.table()
